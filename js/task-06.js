437f804f61ceb0f70635845f1811e3f81684a516
Vitalii Bielov <vitaliybelov.bvs@gmail.com>
2022-11-04 23:11:56 +0200

    done

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#validation-input');
+// inputEl.classList.add('valid')
+
+
+const checkSymbQnt = event => {
+    inputEl.classList.add(event.target.value.length >= inputEl.dataset.length ? 'valid' : 'invalid');
+}
+
+const onFocusInput = () => {
+    inputEl.classList.remove('valid', 'invalid')
+}
+
+inputEl.addEventListener('blur', checkSymbQnt)
+inputEl.addEventListener('focus', onFocusInput)
