7996ce49182841207d8a0d102a6bb4a0a5d9dc99
Vitalii Bielov <vitaliybelov.bvs@gmail.com>
2022-11-05 00:22:10 +0200

    done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const bodyEl = document.querySelector('body');
+const colorValue = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+
+const changeBgColor = () => {
+  const randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  colorValue.innerHTML = randomColor;
+}
+
+
+changeColorBtn.addEventListener('click', changeBgColor)
