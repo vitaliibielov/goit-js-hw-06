f4b7c0ed0c9dd4c9dea473dfee33de5b655d10e9
Vitalii Bielov <vitaliybelov.bvs@gmail.com>
2022-11-04 20:28:47 +0200

    second task done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,25 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingrList = document.querySelector('#ingredients');
+
+// ingredients.forEach(ingredient => {
+//   const ingrEl = document.createElement('li');
+//   ingrEl.textContent = ingredient;
+//   ingrEl.classList.add('item');
+//   ingrList.append(ingrEl);
+// })
+
+
+
+
+
+const liEls = ingredients.map(ingredient => {
+  const ingrEl = document.createElement('li');
+  ingrEl.textContent = ingredient;
+  ingrEl.classList.add('item');
+  return ingrEl;
+})
+
+ingrList.append(...liEls);
